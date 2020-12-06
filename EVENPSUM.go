cc1a0e2244d497684eee9465b9f7583fa6619ddb
Rohit Patil <rahulhimesh09@gmail.com>
2020-12-06 07:45:45 +0530

    dotn bruteforce for high constraints ;_;

--- EVENPSUM.go
+++ EVENPSUM.go
@@ -4,21 +4,32 @@ import (
 	"fmt"
 )
 
+func findOdd(n int) (odd int) {
+	var isNOdd bool
+	if n%2 == 0 {
+		isNOdd = false
+	} else {
+		isNOdd = true
+	}
+	if isNOdd == true {
+		odd = n/2 + 1
+	} else {
+		odd = n / 2
+	}
+	return odd
+}
 func main() {
 	var testCases, A, B, counter int
 	fmt.Scanf("%d", &testCases)
 	for i := 0; i < testCases; i++ {
 		counter = 0
 		fmt.Scanf("%d %d", &A, &B)
-		for x := 1; x <= A; x++ {
-			for y := 1; y <= B; y++ {
-				//				fmt.Println("(", x, ",", y, ")")
-				if (x+y)%2 == 0 {
-					//					fmt.Println("increase counter")
-					counter++
-				}
-			}
-		}
+		oddA := findOdd(A)
+		oddB := findOdd(B)
+		evenA := A - oddA
+		evenB := B - oddB
+		counter = (oddA * oddB) + (evenA * evenB)
+
 		fmt.Println(counter)
 	}
 }
