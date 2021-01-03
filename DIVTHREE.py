bdc61924436a883acd925e538ddf5e3e81764dd2
Rohit Patil <rahulhimesh09@gmail.com>
2021-01-03 21:12:08 +0530

    aC solution using simple maths lmao

--- DIVTHREE.py
+++ DIVTHREE.py
@@ -3,12 +3,8 @@ test_cases = int(input())
 for _ in range(test_cases):
     _, K, D = map(int, input().strip().split())
     total = sum(map(int, input().strip().split()))
-    counter = 0
-    while total >= K:
-        counter += 1
-        total -= K
-        if counter >= D:
-            print(D)
-            break
+    counter = total//K
+    if counter >= D:
+        print(D)
     else:
         print(counter)
