95768ee2e8c7501df670b6b367489d5de402b04d
Rohit Patil <rahulhimesh09@gmail.com>
2021-01-04 16:21:49 +0530

    NZEC on second tast and TLE on subtask 1

---
+++ FAIRELCT.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func max(x []int) int {
+	curmax := x[0]
+	curmaxIdx := 0
+	for i, v := range x {
+		if v > curmax {
+			curmaxIdx = i
+		}
+	}
+	return curmaxIdx
+}
+
+func min(x []int) int {
+	curmin := x[0]
+	curminIdx := 0
+	for i, v := range x {
+		if v < curmin {
+			curminIdx = i
+		}
+	}
+	return curminIdx
+}
+func swap(x []int, y []int, a int, b int) {
+	x[a], y[b] = y[b], x[a]
+}
+func sum(x []int) int {
+	total := 0
+	for _, v := range x {
+		total += v
+	}
+	return total
+}
+func main() {
+	var testCases int
+	fmt.Scanf("%d\n", &testCases)
+	for i := 0; i < testCases; i++ {
+		var N, M int
+		fmt.Scanf("%d %d\n", &N, &M)
+		A := make([]int, N)
+		for i := range A {
+			fmt.Scanf("%d", &A[i])
+		}
+		fmt.Scanf("\n")
+		B := make([]int, M)
+		for i := range B {
+			fmt.Scanf("%d", &B[i])
+		}
+		fmt.Scanf("\n")
+		var counter int
+		for sum(A) <= sum(B) {
+			counter++
+			swap(A, B, min(A), max(B))
+		}
+		fmt.Println(counter)
+	}
+}
