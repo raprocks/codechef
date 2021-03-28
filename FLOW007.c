b88c38ec16ceca6f6a3c553b44975866cde744f3
Rohit Patil <rahulhimesh09@gmail.com>
2021-03-28 16:04:13 +0530

    FLOW007 in C

--- FLOW007.c
+++ FLOW007.c
@@ -2,17 +2,23 @@
 
 int main(int argc, char const *argv[])
 {
-    int input, power = 1, rev = 0, cur;
-    scanf("%d", &input);
-    while (input != 0)
+    int testCases, input, rev = 0, cur;
+    scanf("%d", &testCases);
+    for (int i = 0; i < testCases; i++)
     {
-        cur = input % 10;
-        rev = (rev * 10) + cur;
-        power++;
-        input /= 10;
-        // printf("%d\n", rev);
+        rev = 0;
+        cur = 0;
+
+        scanf("%d", &input);
+        while (input != 0)
+        {
+            cur = input % 10;
+            rev = (rev * 10) + cur;
+            input /= 10;
+            // printf("%d\n", rev);
+        }
+        printf("%d\n", rev);
     }
-    printf("%d", rev);
 
     return 0;
 }
